c196eb8ffcf67e6b625790e07b1d7dea9e0e4808
Ashley Hong <ayhong8@gmail.com>
2024-03-21 14:26:14 -0400

    Remove raw partner analysts data

--- pipeline/main.py
+++ pipeline/main.py
@@ -10,9 +10,9 @@ from scrapers.benzinga_scraper import BenzingaScraper
 if __name__ == '__main__':
     load_dotenv()
     ### StockTwitsParser
-    columns = ["created_at", "body_prepared", "ticker", "source", "sentiment"]
-    parser = StockTwitsParser(columns, ticker="NVDA", src="./data/StockTwits/NVDA_pre_processed.csv", dest="./output/nvda_stocktwits_final.csv")
-    parser.parse()
+    # columns = ["created_at", "body_prepared", "ticker", "source", "sentiment"]
+    # parser = StockTwitsParser(columns, ticker="NVDA", src="./data/StockTwits/NVDA_pre_processed.csv", dest="./output/nvda_stocktwits_final.csv")
+    # parser.parse()
 
     # scraper = AlphaVantageScraper()
     # scraper.scrape_stocks("NVDA", "2010-01-01", "2020-01-01")
@@ -20,11 +20,8 @@ if __name__ == '__main__':
     # scraper = BenzingaScraper()
     # scraper.scrape_news("AAPL", "2021-01-01", "2021-01-10")
 
-    # analyst_parser = NewsHeadlinesParser(["date", "title", "stock", "source"], src="./data/News_Headlines/analyst_ratings_processed.csv", dest="./output/nvda_news_headlines.csv", ticker="NVDA")
-    # analyst_parser.parse()
-    #
-    # headline_parser = NewsHeadlinesParser(["date", "headline", "stock", "source"], src="./data/News_Headlines/raw_partner_headlines.csv", dest="./output/nvda_news_headlines.csv", ticker="NVDA")
-    # headline_parser.parse()
+    analyst_parser = NewsHeadlinesParser(["date", "title", "stock", "source"], src="./data/News_Headlines/analyst_ratings_processed.csv", dest="./output/nvda_news_headlines.csv", ticker="NVDA")
+    analyst_parser.parse()
 
     # combine_parser = CombineDataParser(ticker="NVDA", src="./data/precombined", dest="./output", start_date="2014-01-01", end_date="2020-01-01")
     # combine_parser.parse()
